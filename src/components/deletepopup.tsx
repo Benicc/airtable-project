e4433deb5a3c713fa4582c2afb7a2f6163371e74
Benicc <ben.pamittan@gmail.com>
2024-12-17 15:40:54 +1100

    fix linting errors

--- src/components/deletepopup.tsx
+++ src/components/deletepopup.tsx
@@ -37,7 +37,7 @@ const DeletePopup: React.FC<PopupProps> = ({ isVisible, onClose, baseId, baseNam
       className="fixed inset-0 bg-black bg-opacity-20 flex justify-center items-center z-50"
     >
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-        <h2 className="text-xl mb-4">Are you sure you want to delete "{baseName}" base?</h2>
+        <h2 className="text-xl mb-4">Are you sure you want to delete &quot;{baseName}&quot; base?</h2>
         <div className='flex justify-between'>
             <button
             className="border border-2  px-4 py-2"
